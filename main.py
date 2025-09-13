49899f46ed1558226bcc5a349443c1ab8b8995bc
JRichardBrown <JRBrown4758@gmail.com>
2025-09-13 15:59:49 -0700

    Made requested changes, including error-handling.

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import sqlite3
 import os
 
 app = FastAPI()
-# run the following command in a Bash window: <uvicorn main:app --reload>
 
 @app.get("/")
 async def root():
+    print("get function called.")
     conn = sqlite3.connect('culture-popper.db')
     cur = conn.cursor()
 
-    res= cur.execute('SELECT reference FROM pc_references ORDER BY RANDOM() LIMIT 1')
-
-    return res.fetchone()
+    try:
+        msg = cur.execute('SELECT reference FROM pc_references ORDER BY RANDOM() LIMIT 1').fetchone()
+    
+    except Exception as e:
+        msg = f"An error occured during a call to the 'get' function: {e}"
+    
+    return msg
